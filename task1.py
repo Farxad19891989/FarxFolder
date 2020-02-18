fc65ebaebdf4de2e2ba700e13bec3e370b267978
Farxad19891989 <farxadrzayev24@gmail.com>
2020-02-18 19:10:48 +0400

    Tesdiq

---
+++ task1.py
@@ -0,0 +1,34 @@
+#name=input("Enter your name\n")
+#print("Welcome",name)
+
+
+#hours=input("Enter hours\n")
+#rate=input("Enter rate\n")
+#pay=int(hours) * float(rate)
+#print("Pay:",pay)
+
+#width=17
+#height=12.0
+
+#print("The value",width//2)
+#print("The type",type(width//2))
+
+#print("The value",width/2.0)
+#print("The type",type(width/2))
+
+#print("The value",height/3)
+#print("The type",type(height/3))
+
+#print("The value",1+2*5)
+#print("The type",type(1+2*5))
+
+
+
+#celsius=input("Enter degree in celsius\n")
+#farenheit=int(celsius) * 9/5 +32
+#print(celsius," Celsius is",farenheit,"Farenheits")
+
+
+
+
+
